bd0c60a7ae7eedd0c3687d6030a4942f17ce3aea
Ashit Chuphal <chuphalashit@gmail.com>
2026-07-27 19:38:25 +0400

    Updare Client URL for the ecs task definitions

--- infrastructure/terraform/ecs-task-definitions.tf
+++ infrastructure/terraform/ecs-task-definitions.tf
@@ -37,11 +37,11 @@ resource "aws_ecs_task_definition" "backend" {
         },
         {
           name  = "CLIENT_URL"
-          value = "https://careercopilot-alb-2135176287.us-east-1.elb.amazonaws.com"
+          value = "https://careercopilotpro.com"
         },
         {
           name  = "GOOGLE_CALLBACK_URL"
-          value = "https://careercopilot-alb-2135176287.us-east-1.elb.amazonaws.com/auth/google/callback"
+          value = "https://careercopilotpro.com/auth/google/callback"
         }
       ]
 
